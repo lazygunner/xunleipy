1cc8c7ed2a381eaac7277931c29ea2844d9abad5
gunner <gymgunner@gmail.com>
2015-03-21 17:24:04 +0800

    Add ruokuai verification crack

--- xunleipy/base.py
+++ xunleipy/base.py
@@ -4,6 +4,7 @@ from time import time, sleep
 import requests
 
 from .utils import get_password_hash
+from .rk import RClient
 
 
 DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0)\
@@ -13,12 +14,13 @@ DEFAULT_REFERER = 'http://i.vod.xunlei.com/resource_assistant'
 
 class XunLei(object):
 
-    def __init__(
-            self,
-            username=None,
-            password=None,
-            user_agent=DEFAULT_USER_AGENT,
-            referer=DEFAULT_REFERER):
+    def __init__(self,
+                 username=None,
+                 password=None,
+                 user_agent=DEFAULT_USER_AGENT,
+                 referer=DEFAULT_REFERER,
+                 rk_username=None,
+                 rk_password=None):
 
         self.username = username
         self.password = password
@@ -29,9 +31,29 @@ class XunLei(object):
         self.is_login = False
         self.session_id = None
         self.lsession_id = None
+        if rk_password and rk_password:
+            self.rk_client = RClient(rk_username, rk_password)
+        else:
+            self.rk_client = None
 
     def _current_timestamp(self):
         return int(time() * 1000)
+
+    def _crack_verify_code(self):
+        url = 'http://verify2.xunlei.com/image?t=MVA&cachetime=%s' % self._current_timestamp()
+        r = self.get(url, stream=True)
+        if r.status_code == 200:
+            image = r.content
+            if self.rk_client:
+                verify_code = self.rk_client.rk_create(image, 3040)
+            else:
+                print ('need login ruokuai account')
+                return None
+        else:
+            print ('get verify code error')
+            return None
+
+        return verify_code
 
     def login(self):
         login_url = 'http://login.xunlei.com/sec2login/'
@@ -52,7 +74,9 @@ class XunLei(object):
                 break
             else:
                 print ('verify_code:', r.cookies.get('check_result', ''))
-                verify_code = ''
+                verify_code = self._crack_verify_code()
+                if verify_code:
+                    break
                 try_time += 1
                 sleep(10)
 
---
+++ xunleipy/rk.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import requests
+from hashlib import md5
+
+
+class RClient(object):
+
+    def __init__(self,
+                 username,
+                 password,
+                 soft_id='37093',
+                 soft_key='72e005a3a3f2480c900c32a2ac2660d5'):
+
+        self.username = username
+        self.password = md5(password).hexdigest()
+        self.soft_id = soft_id
+        self.soft_key = soft_key
+        self.base_params = {
+            'username': self.username,
+            'password': self.password,
+            'softid': self.soft_id,
+            'softkey': self.soft_key,
+        }
+        self.headers = {
+            'Connection': 'Keep-Alive',
+            'Expect': '100-continue',
+            'User-Agent': 'ben',
+        }
+
+    def rk_create(self, im, im_type, timeout=60):
+        """
+        im: 图片字节
+        im_type: 题目类型
+        """
+        params = {
+            'typeid': im_type,
+            'timeout': timeout,
+        }
+        params.update(self.base_params)
+        files = {'image': ('check_code.png', im)}
+        r = requests.post('http://api.ruokuai.com/create.json', data=params, files=files, headers=self.headers)
+        return r.json()
+
+    def rk_report_error(self, im_id):
+        """
+        im_id:报错题目的ID
+        """
+        params = {
+            'id': im_id,
+        }
+        params.update(self.base_params)
+        r = requests.post('http://api.ruokuai.com/reporterror.json', data=params, headers=self.headers)
+        return r.json()
