ddafdfee1cf874365bd9445a3474265dc34823da
gunner <gymgunner@gmail.com>
2017-04-16 23:35:22 +0800

    Update Js2Py version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='xunleipy',
-    version='0.3.2',
+    version='0.3.3',
     author='lazygunner',
     author_email='gymgunner@gmail.com',
     url='https://github.com/lazygunner/xunleipy',
